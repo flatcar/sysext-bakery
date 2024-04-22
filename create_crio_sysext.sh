32dac6b16739fd43941a3027815a031c526949f6
Kai Lüke <pothos@users.noreply.github.com>
2024-04-22 05:29:12 +0200

    Update create_crio_sysext.sh

--- create_crio_sysext.sh
+++ create_crio_sysext.sh
@@ -35,7 +35,7 @@ cd ${SYSEXTNAME}/tmp/cri-o/
 sed -i '/^sed -i.*DESTDIR/d' install # removes sed replacements from install script to keep the default location (/usr) in the base config file
 DESTDIR="${PWD}/../../../${SYSEXTNAME}" PREFIX=/usr ETCDIR=$PREFIX/share/crio/etc OCIDIR=$PREFIX/share/oci-umount/oci-umount.d \
   CNIDIR=$PREFIX/share/crio/cni/etc/net.d/ OPT_CNI_BIN_DIR=$PREFIX/share/crio/cni/bin/  BASHINSTALLDIR=/tmp FISHINSTALLDIR=/tmp ZSHINSTALLDIR=/tmp MANDIR=/tmp ./install 
-cd "${SCRIPTFOLDER}"
+cd -
 rm -rf "${SYSEXTNAME}/tmp" 
 
 
