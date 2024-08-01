81f6814d1ca782cdb3490d9bbecd8d8f3381d790
salatiel <salatiel@none>
2024-08-01 10:50:34 -0300

    Improve vimrc defaults

--- create_vim-huge_sysext.sh
+++ create_vim-huge_sysext.sh
@@ -45,5 +45,32 @@ EOF
 chmod +x "${SYSEXTNAME}"/build.sh
 docker run -v "${PWD}/${SYSEXTNAME}":/install_root/  --rm "${IMG}" /bin/sh -c /install_root/build.sh
 rm -f "${SYSEXTNAME}"/build.sh
+cat > "${SYSEXTNAME}"/usr/share/vim/vimrc  <<-"EOF"
+" Vim will load $VIMRUNTIME/defaults.vim if the user does not have a vimrc.
+" This happens after /etc/vim/vimrc(.local) are loaded, so it will override
+" any settings in these files.
+" Thus we source the default and disable load defaults afterwords so we can override
+" options in these files.
+" Use :verbose set $option? or :scriptnames to see the where any option is being set from
+if filereadable("/usr/share/vim/vim90/defaults.vim")
+  source /usr/share/vim/vim90/defaults.vim
+endif
+let g:skip_defaults_vim = 1
+
+" Read vimrc.local from /etc if it exists. (read-write root images)
+if filereadable("/etc/vim/vimrc.local")
+  source /etc/vim/vimrc.local
+endif
+
+" Useful config for editing yaml files 
+set ai ts=2 sw=2 et
+set pastetoggle=<F3>
+set ignorecase
+set mouse-=a
+inoremap <S-Tab> <C-d>
+map <silent> <F5> :set cursorcolumn!<CR>
+set maxmempattern=50000
+EOF
+
 RELOAD=1 "${SCRIPTFOLDER}"/bake.sh "${SYSEXTNAME}"
 rm -rf "${SYSEXTNAME}"
