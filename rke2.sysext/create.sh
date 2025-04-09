346d28bf3ba22431ec82e2c4afdc2e3cda0481b6
Thilo Fromm <thilofromm@microsoft.com>
2025-04-09 15:09:32 +0200

    k3s,kubernetes,rke2: fix sysupdate symlink names
    We publish k3s, kubernetes, and rke2 with special sysupdate configs to
    ensure only patch levels are updated. However, we still use <extension>
    w/o major version as the sysext's name, and we use a matching symlink
    name.
    
    The special sysupdate configs however use
    <extension>-<majorversion>.raw.
    This change updates sysupdate configuration generation for the 3
    extensions to use the correct symlink.
    
    Fixes #140.
    
    Signed-off-by: Thilo Fromm <thilofromm@microsoft.com>

--- rke2.sysext/create.sh
+++ rke2.sysext/create.sh
@@ -33,7 +33,7 @@ function populate_sysext_root() {
   local sysupdate="$(get_optional_param "sysupdate" "false" "${@}")"
   if [[ ${sysupdate} == true ]] ; then
     local ver="$(echo "${version}" | sed 's/^\(v[0-9]\+\.[0-9]\+\).*/\1/')"
-    _create_sysupdate "${extname}-${ver}" "${extname}-${ver}.@v-%a.raw" "${extname}" "${extname}"
+    _create_sysupdate "${extname}" "${extname}-${ver}.@v-%a.raw" "${extname}" "${extname}" "${extname}-${ver}.conf"
     mv "${extname}-${ver}.conf" "${rundir}"
   fi
 }
