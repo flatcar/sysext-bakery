6e30a4a46137f7c06ad340361661317c23aa515d
fl0eb <5031220+fl0eb@users.noreply.github.com>
2025-04-22 01:25:57 +0200

    Move metadata to create.sh

--- gasket.sysext/create.sh
+++ gasket.sysext/create.sh
@@ -5,14 +5,14 @@
 #
 
 RELOAD_SERVICES_ON_MERGE="true"
+ORG="KyleGospo"
+PROJECT="gasket-dkms"
 
 function list_available_versions() {
   # Gasket does not have any tags nor releases.
   # As a replacement, we use the branch names.
-  local org="KyleGospo"
-  local project="gasket-dkms"
   curl_api_wrapper \
-      "https://api.github.com/repos/${org}/${project}/branches" \
+      "https://api.github.com/repos/${ORG}/${PROJECT}/branches" \
       | jq -r '.[].name' \
       | sort -Vr
 }
@@ -21,6 +21,12 @@ function populate_sysext_root() {
   local sysextroot="$1"
   local arch="$2"
   local version="$3"
+
+  mkdir -p "${sysextroot}/usr/share/flatcar"
+    cat <<EOF >"${sysextroot}/usr/share/flatcar/gasket-metadata"
+GASKET_REPOSITORY=https://github.com/${ORG}/${PROJECT}.git
+GASKET_BRANCH=${version}
+EOF
 
   chmod +x "${sysextroot}/usr/lib/gasket/bin/install-gasket"
   chmod +x "${sysextroot}/usr/lib/gasket/bin/setup-gasket"
