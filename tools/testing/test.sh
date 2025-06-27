cc805d0cefe02e2b0fdc539cf32c9159e3d8f953
Thilo Fromm <thilofromm@microsoft.com>
2025-06-27 16:25:33 +0200

    Apply suggestions from code review
    Co-authored-by: James Le Cuirot <jlecuirot@microsoft.com>

--- tools/testing/test.sh
+++ tools/testing/test.sh
@@ -23,7 +23,7 @@ function download_if_needed() {
 
   local f
   for f in "${files[@]}"; do
-    if [ ! -e "$f" ] ; then
+    if [[ ! -e $f ]] ; then
       download="true"
     fi
   done
@@ -34,10 +34,7 @@ function download_if_needed() {
 
   echo "Flatcar image not found, downloading."
 
-  for f in "${files[@]}"; do
-    rm -f "$f"
-    curl -fL --progress-bar --retry-delay 1 --retry 60 --retry-connrefused --remote-name "https://stable.release.flatcar-linux.net/amd64-usr/current/$f"
-  done
+curl -fL --progress-bar --retry-delay 1 --retry 60 --retry-connrefused --remote-name "${files[@]/#/https://stable.release.flatcar-linux.net/amd64-usr/current/}"
 
   chmod 755 flatcar_production_qemu_uefi.sh
 }
@@ -48,17 +45,17 @@ function webserver() {
   cd "${path}"
   exec docker run -i --rm \
       -p 8000:80 \
-      -v "$(pwd)":/usr/share/caddy \
+      -v "${PWD}":/usr/share/caddy \
   caddy
 }
 # --
 
 download_if_needed
 
-sed -s "s/EXTENSION/${extension}/g" "test.yaml.tmpl" > "test.yaml"
+sed "s/EXTENSION/${extension}/g" "test.yaml.tmpl" > "test.yaml"
 cat test.yaml \
     | docker run --rm \
-      -v "$(pwd)":/files \
+      -v "${PWD}":/files \
       -i quay.io/coreos/butane:latest \
       --files-dir /files  \
     > test.json
