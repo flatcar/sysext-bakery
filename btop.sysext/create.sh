60e2b71b5f5cfa20ece3b919513fb852ab78f653
Kai Lueke <kailuke@microsoft.com>
2025-10-21 22:03:33 +0900

    Add example extensions built with flix.sh and flatwrap.sh
    To show how flix.sh and flatwrap.sh can be used to ship regular Linux
    packages as sysext we can add two examples.
    As flix example include an extension for the tilde text editor from
    Debian and as flatwrap example include an extension for btop from
    Alpine (originally I used htop but we might ship this in Flatcar later
    by default - note that you need to set LANG=en_US.UTF-8 or run with btop
    with --utf-force on Flatcar).
    
    Signed-off-by: Kai Lueke <kailuke@microsoft.com>

---
+++ btop.sysext/create.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# vim: et ts=2 syn=bash
+#
+# btop sysext.
+#
+
+RELOAD_SERVICES_ON_MERGE="false"
+
+function list_available_versions() {
+  # Add sort for consuming all curl output
+  # (When copying this, you might need to switch to 'main' from 'community')
+  curl -sSfL https://dl-cdn.alpinelinux.org/alpine/latest-stable/community/x86_64/ | { sort || true ; } | grep -m1 -o "btop-.*apk" | cut -d - -f 2
+}
+# --
+
+function populate_sysext_root() {
+  local sysextroot="$1"
+  local arch="$2"
+  local version="$3"
+
+  local img_arch="$(arch_transform 'x86-64' 'amd64' "$arch")"
+  img_arch="$(arch_transform 'arm64' 'arm64/v8' "$img_arch")"
+
+  docker run --rm \
+              -i \
+              -v "${scriptroot}/tools/":/tools \
+              -v "${sysextroot}":/install_root \
+              --platform "linux/${img_arch}" \
+              --pull always \
+              --network host \
+              docker.io/alpine:latest \
+                  sh -c "apk add -U btop bash coreutils grep && cd /install_root && ETCMAP=chroot /tools/flatwrap.sh / btop /usr/bin/btop"
+  # Alpine has /etc/terminfo instead of /usr/terminfo,
+  # so above we need to skip mapping the host /etc into the flatwrap env
+  mv "${sysextroot}"/btop/usr "${sysextroot}"/usr
+  rmdir "${sysextroot}"/btop
+  # Workaround: The bakery.sh tmp dir cleanup fails otherwise
+  chmod -R u+w "${sysextroot}" || true
+}
+# --
