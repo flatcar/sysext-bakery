2b8402fb71c9243d04ae9f658ec197db733b500f
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-12-03 15:17:22 +0100

    nvidia-runtime: fix v1.18.1 build with patch
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- nvidia-runtime.sysext/create.sh
+++ nvidia-runtime.sysext/create.sh
@@ -26,6 +26,11 @@ function populate_sysext_root() {
 
   git clone -b ${version} --depth 1 https://github.com/NVIDIA/libnvidia-container
   git clone -b ${version} --depth 1 https://github.com/NVIDIA/nvidia-container-toolkit
+  if [ "${version}" == v1.18.1 ]; then
+    pushd nvidia-container-toolkit
+    patch -p1 <"${scriptroot}/nvidia-runtime.sysext/0001-nvidia-runtime-1.18.1-go-dl.patch"
+    popd
+  fi
 
   make -C libnvidia-container ubuntu18.04-${rel_arch}
   make -C nvidia-container-toolkit ubuntu18.04-${rel_arch}
