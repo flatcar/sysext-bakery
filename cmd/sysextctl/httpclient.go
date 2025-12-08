d616877db05821041968990305cb04a810035136
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-12-08 12:25:33 +0100

    sysextctl: Remove ratelimit header handling

--- cmd/sysextctl/httpclient.go
+++ cmd/sysextctl/httpclient.go
@@ -27,36 +27,10 @@ func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) 
 	if rt == nil {
 		rt = http.DefaultTransport
 	}
-	method := req.Method
-	if method == "" {
-		method = http.MethodGet
-	}
-	fmt.Fprintf(os.Stderr, "%s %s\n", method, req.URL.String())
+	fmt.Fprintf(os.Stderr, "%s %s\n", req.Method, req.URL.String())
 	resp, err := rt.RoundTrip(req)
 	if err != nil {
 		return nil, err
 	}
-	printRateLimitHeaders(resp)
 	return resp, nil
-}
-
-func printRateLimitHeaders(resp *http.Response) {
-	if resp == nil || resp.Header == nil {
-		return
-	}
-	report := []struct {
-		Name string
-		Key  string
-	}{
-		{"X-RateLimit-Limit", "X-RateLimit-Limit"},
-		{"X-RateLimit-Remaining", "X-RateLimit-Remaining"},
-		{"X-RateLimit-Used", "X-RateLimit-Used"},
-		{"X-RateLimit-Resource", "X-RateLimit-Resource"},
-		{"X-RateLimit-Reset", "X-RateLimit-Reset"},
-	}
-	for _, item := range report {
-		if value := resp.Header.Get(item.Key); value != "" {
-			fmt.Fprintf(os.Stderr, "  %s: %s\n", item.Name, value)
-		}
-	}
 }
